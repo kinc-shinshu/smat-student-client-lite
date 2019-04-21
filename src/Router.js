725d839438992d72d938afc5e8c6ab32cf09167c
yoidea <yoidea.cc@gmail.com>
2019-04-21 19:50:24 +0900

    :heavy_plus_sign: Add context API

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "./Context";
 import Home from "./pages/Home";
 import Exam from "./pages/Exam";
 import Question from "./pages/Question";
@@ -8,7 +9,7 @@ class Router extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <Provider value={"Hello World"}>
           <Route exact path="/" component={Home} />
           <Route exact path="/exams/:id(\d+)" component={Exam} />
           <Route
@@ -16,7 +17,7 @@ class Router extends Component {
             path="/exams/:exam_id(\d+)/questions/:id(\d+)"
             component={Question}
           />
-        </div>
+        </Provider>
       </BrowserRouter>
     );
   }

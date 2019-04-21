1c49399db63d58844595a1f092bd1264ef8b85f7
yoidea <yoidea.cc@gmail.com>
2019-04-21 20:17:55 +0900

    :sparkles: Add back links

--- src/pages/Exam.js
+++ src/pages/Exam.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Consumer } from "../Context";
+import { Link } from "react-router-dom";
 import "react-materialize";
 
 class Home extends Component {
@@ -8,6 +9,7 @@ class Home extends Component {
       <div className="App">
         <h1>Exam {this.props.match.params.id}</h1>
         <Consumer>{({ text }) => <p>Context: {text}</p>}</Consumer>
+        <Link to="/">Back</Link>
       </div>
     );
   }
